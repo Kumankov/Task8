274a2dba244ced96fa9d07af7e8a7115c4b1a0d7
Ilya <kumankov@gmail.ru>
2022-11-18 23:15:40 +0300

    12

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ Console.WriteLine("введите число:");
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+while (i<=N)
+{
+    if ((i % 2)==0)
+    {    
+    if (i>2) {Console.Write(", ");} // пишем запятую перед каждым числом, кроме первого
+    Console.Write(i);
+    }
+    i++;
+}
